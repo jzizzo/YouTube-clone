c3504b9dcdc8c75132cc5e8501fc3b27f5d091f9
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-28 18:07:15 -0700

    display video player and video list details

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 
@@ -21,6 +23,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
