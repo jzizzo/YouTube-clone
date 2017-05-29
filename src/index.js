25c9614b9f9410be87ed87dfd1165c1ef56d2160
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-28 22:38:40 -0700

    add styling and debounce search calls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,9 +16,13 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('surfboards')
+  }
 
 
-    YTSearch({ key: window.API_KEY, term: 'surfboards'}, (videos) => {
+
+  videoSearch(term) {
+    YTSearch({ key: window.API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -26,9 +31,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
         onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
